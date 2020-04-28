cf2f3f1b65d7e05aff0c9aaaa2898b2c411abc9a
Kaiting Chen <ktchen14@gmail.com>
2020-04-28 16:17:28 -0700

    Add aerate

---
+++ aerate/aerate.py
@@ -0,0 +1,18 @@
+from aerate.adjust import adjuster
+from aerate.index import Index
+from aerate.render import renderer
+
+
+class Aerate:
+    def __init__(self, root):
+        self.index = Index(root)
+        self.index.aerate = self
+
+        self.adjuster = adjuster
+        self.adjuster.aerate = self
+
+        self.renderer = renderer
+        self.renderer.aerate = self
+
+    def render(self, node):
+        return self.renderer.render(node)
