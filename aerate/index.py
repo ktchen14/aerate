073c99c37090caa2f381313fbe05bd0189cc2c2b
Kaiting Chen <ktchen14@gmail.com>
2020-04-28 12:44:58 -0700

    Flesh out index

--- aerate/index.py
+++ aerate/index.py
@@ -6,34 +6,62 @@ from lxml.etree import XMLParser
 
 
 class DocumentableObject:
-    def __init__(self, node):
-        self.node = node
+    def __init__(self, index, index_node):
+        self.index = index
+        self.index_node = index_node
+        self._node = None
 
     @property
     def name(self):
-        return self.node.find("name").text
+        return self.index_node.find("name").text
 
     @property
     def id(self):
-        return self.attrib["id"]
+        return self.index_node.attrib["refid"]
 
     @property
     def kind(self):
-        return self.attrib["kind"]
+        return self.index_node.attrib["kind"]
+
+    @property
+    def node(self):
+        if self._node is None:
+            compound_node = self.index_node
+            if self.index_node.tag == "member":
+                compound_node = compound_node.getparent()
+
+            relative_path = compound_node.attrib["refid"] + ".xml"
+
+            document = self.index.get_document(relative_path)
+            if self.index_node.tag == "compound":
+                result = document.xpath(r'//compounddef[@id=$refid and @kind=$kind]',
+                                        refid=self.id, kind=self.kind)
+            elif self.index_node.tag == "member":
+                result = document.xpath(r'//memberdef[@id=$refid and @kind=$kind]',
+                                        refid=self.id, kind=self.kind)
+            self._node = result[0]
+        return self._node
 
 
-class Index(collections.abc.Mapping):
+class Index:
     def __init__(self, doxygen_root):
         self.doxygen_root = doxygen_root
 
-        parser = XMLParser(ns_clean=True,
-                           remove_blank_text=True,
-                           remove_comments=True,
-                           remove_pis=True,
-                           strip_cdata=True)
+        index_path = os.path.join(doxygen_root, "index.xml")
+        self.parser = XMLParser(ns_clean=True,
+                                remove_blank_text=True,
+                                remove_comments=True,
+                                remove_pis=True,
+                                strip_cdata=True)
+        self.document = etree.parse(index_path, self.parser)
+        self.memo = {"index.xml": self.document}
 
-        with open(os.path.join(doxygen_root, "index.xml")) as file:
-            self.document = etree.parse(file, parser)
+    def get_document(self, name):
+        if name not in self.memo:
+            path = os.path.join(self.doxygen_root, name)
+            document = etree.parse(path, self.parser)
+            self.memo[name] = document
+        return self.memo[name]
 
     def find(self, kind, id):
         # kind must be either "compound" or "member"
@@ -43,20 +71,33 @@ class Index(collections.abc.Mapping):
                 result = result[0]
         else:
             result = self.document.xpath("//member[@refid=$refid]", refid=id)
-
-        if len(result) > 1:
-            result = result[0]
+            if len(result) > 1:
+                result = result[0]
         return result.get("kind"), result.xpath("./name")[0].text
 
-    def __getitem__(self, id):
-        result = self.document.xpath("//*[@refid=$refid]", refid=id)
+    def find_module_by_name(self, name):
+        result = self.document.xpath(r'//compound[name/text()=$name]',
+                                     name=name)
         if not result:
-            raise KeyError(repr(id))
+            raise KeyError(repr(name))
+        elif len(result) > 1:
+            raise KeyError(repr(name))
 
-        # A single object can be in multiple places in Doxygen. e.g.:
-        # <compound refid="vector_2common_8h" kind="file"><name>common.h</name>
-        #   <member refid="vector_2common_8h_1a269b0ad6ebf33c6a47a1f939d6e94e0a" kind="typedef"><name>blah</name></member>
-        #   <member refid="group__vector__module_1ga5b7d6c2eb384dea49554e889ad2740ad" kind="typedef"><name>vector_t</name></member>
-        # </compound>
-        documentable_object = DocumentableObject(result[0])
-        return documentable_object
+        return DocumentableObject(self, result[0])
+
+    def find_member_by_name(self, name, module=None):
+        if module is None:
+            search_root = self.document
+        elif not isinstance(module, DocumentableObject):
+            search_root = self.find_module_by_name(module).index_node
+        else:
+            search_root = module.index_node
+
+        result = search_root.xpath(r'.//member[name/text()=$name]', name=name)
+
+        if not result:
+            raise KeyError(repr(name))
+        elif len(result) > 1:
+            raise KeyError(repr(name))
+
+        return DocumentableObject(self, result[0])
