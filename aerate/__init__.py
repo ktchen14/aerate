5da26faa3d6e7b74a592ce039b54fb573b0d827c
Kaiting Chen <ktchen14@gmail.com>
2020-04-29 21:55:15 -0700

    Add an option

--- aerate/__init__.py
+++ aerate/__init__.py
@@ -6,7 +6,13 @@ import os
 def setup(sphinx):
     sphinx.setup_extension("sphinx.ext.autodoc")
 
-    sphinx.aerate = Aerate(os.path.join(sphinx.confdir, "xml"))
+    # The directory where Doxygen's XML output is. This should be the same as
+    # the XML_OUTPUT configuration option in Doxygen.
+    sphinx.add_config_value("aerate_doxygen_root",
+                            os.path.join(sphinx.confdir, "xml"),
+                            "env")
+
+    sphinx.aerate = Aerate(sphinx.config.aerate_doxygen_root)
     sphinx.add_autodocumenter(FunctionDocumenter)
 
     return {"version": "0.0.1", "parallel_read_safe": True}
