c80a408e0abadba6f2b443485363fd97ac89ae01
Kaiting Chen <ktchen14@gmail.com>
2020-04-29 21:50:10 -0700

    Better setup

--- aerate/__init__.py
+++ aerate/__init__.py
@@ -4,11 +4,9 @@ import os
 
 
 def setup(sphinx):
+    sphinx.setup_extension("sphinx.ext.autodoc")
+
     sphinx.aerate = Aerate(os.path.join(sphinx.confdir, "xml"))
     sphinx.add_autodocumenter(FunctionDocumenter)
 
-    return {
-        "version": "0.0.1",
-        "parallel_read_safe": True,
-        "parallel_write_safe": True,
-    }
+    return {"version": "0.0.1", "parallel_read_safe": True}
