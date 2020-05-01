41299ae84b29401116aeb15f3317ec063157b199
Kaiting Chen <ktchen14@gmail.com>
2020-04-30 22:03:14 -0700

    Remove init from renderer

--- aerate/render.py
+++ aerate/render.py
@@ -2,8 +2,5 @@ from aerate.engine import Engine
 
 
 class Renderer(Engine):
-    def __init__(self, *args, recipe="aerate.recipe.renderer", **kwargs):
-        super().__init__(*args, recipe=recipe, **kwargs)
-
     def on_unaccepted(self, node, *args, **kwargs):
         return node.xpath("string()")
