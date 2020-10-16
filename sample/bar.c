71d7d8655419efe14c9fc7f31bbef60465b04bd1
Kaiting Chen <ktchen14@gmail.com>
2020-10-15 18:16:32 -0700

    Add sample

---
+++ sample/bar.c
@@ -0,0 +1,10 @@
+/// @file bar.c
+
+/// This is an ambiguous struct in bar.c
+struct ambiguous_struct {
+  long member;
+};
+
+/// This is an ambiguous function in bar.c
+void ambiguous_function(void) {
+}
---
+++ sample/foo.c
@@ -0,0 +1,10 @@
+/// @file foo.c
+
+/// This is an ambiguous struct in foo.c
+struct ambiguous_struct {
+  int member;
+};
+
+/// This is an ambiguous function in foo.c
+void ambiguous_function(void) {
+}
