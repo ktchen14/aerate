a8c0e3cb2c96fd4b09e8a43017c555926a7aa378
Kaiting Chen <ktchen14@gmail.com>
2020-10-18 13:51:40 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import codecs
+from setuptools import setup
+
+with codecs.open("README.me", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="aerate",
+    version="0.0.1",
+    description="A Doxygen to Sphinx bridge",
+    long_description=readme,
+    author="Kaiting Chen",
+    author_email="ktchen14@gmail.com",
+    url="https://github.com/ktchen14/aerate",
+    packages=["aerate"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+    ],
+)
