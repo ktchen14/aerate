9280b489664ebb3599f062ed520079489c4f67c6
Kaiting Chen <ktchen14@gmail.com>
2020-10-19 13:38:23 -0700

    Handle formula in adjuster

--- aerate/schema.py
+++ aerate/schema.py
@@ -1,5 +1,6 @@
-# See https://github.com/doxygen/doxygen/blob/master/templates/xml/compound.xsd
+import re
 
+# See https://github.com/doxygen/doxygen/blob/master/templates/xml/compound.xsd
 
 class SchemaError(Exception):
     """Raised when a node is encountered that's unexpected from the schema."""
@@ -9,6 +10,10 @@ DESCRIPTION_TAGS = {
     "briefdescription", "detaileddescription", "inbodydescription",
     "parameterdescription"
 }
+
+# A <formula> node is structural if it begins with \[ or \begin{. Otherwise
+# it's inline. We'll include "formula" in both INLINE_TAGS and STRUCTURAL_TAGS
+# and handle the disambiguation in is_inline() and is_structural().
 
 INLINE_TAGS = {
     "ulink", "bold", "s", "strike", "underline", "emphasis",
@@ -23,6 +28,7 @@ STRUCTURAL_TAGS = {
     "orderedlist", "itemizedlist", "simplesect", "title", "variablelist",
     "table", "heading", "dotfile", "mscfile", "diafile", "toclist", "language",
     "parameterlist", "xrefsect", "copydoc", "blockquote", "parblock",
+    "formula",
 }
 
 
@@ -33,9 +39,13 @@ def is_description(node):
 
 def is_inline(node):
     """Return whether the ``node`` is an inline markup node."""
+    if node.tag == "formula":
+        return not re.match(r"\s*(\\\[|\\begin\{)", node.text)
     return node.tag in INLINE_TAGS
 
 
 def is_structural(node):
     """Return whether the ``node`` is a structural markup node."""
+    if node.tag == "formula":
+        return re.match(r"\s*(\\\[|\\begin\{)", node.text)
     return node.tag in STRUCTURAL_TAGS
